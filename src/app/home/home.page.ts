beccaa5dc6c8f2dda2d51dad0d2b3ff9e23927f4
Natthakit Lekmas <natthakit.lekmas@gmail.com>
2024-08-21 14:18:20 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  constructor(private t: ToastController) {}
+  async presentToast(position: 'bottom') {
+    const toast = await this.t.create({
+      message: 'Hello World!',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 
 }
